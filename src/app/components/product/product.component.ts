5daa908cf50b18b962b98f89138d9ca682ede4bc
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-03-17 22:57:37 +0300

    17 Mart Ders Arası

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,7 +12,9 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) {}
+  filterText = "";
+
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute, private toastrService:ToastrService) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params =>{
@@ -37,5 +40,10 @@ export class ProductComponent implements OnInit {
       this.products = response.data
       this.dataLoaded = true;
     });
+  }
+
+
+  addToCart(product:Product){
+    this.toastrService.success("Sepete eklendi",product.productName);
   }
 }
