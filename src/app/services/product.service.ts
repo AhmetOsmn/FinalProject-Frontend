3385cccab374d841b25e122f54f7028e25d0bc17
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-03-20 23:05:17 +0300

    20 Mart Commit 1

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { listResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 
 @Injectable({
@@ -22,5 +23,10 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<listResponseModel<Product>> {
     let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId;
     return this.httpClient.get<listResponseModel<Product>>(newPath);
-}
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product);
+  }
+
 }
